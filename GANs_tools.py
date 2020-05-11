0e75d8762c2ff311adaef2d0c7b0346fe884bee9
Remi-Boutin <boutin.remi95@gmail.com>
2020-05-11 13:48:36 +0200

    add of 'Wasserstein training"

--- GANs_tools.py
+++ GANs_tools.py
@@ -53,7 +53,7 @@ def zeros_target(size):
     return data
 
 
-def train_discriminator(optimizer, discriminator, loss, real_data, fake_data, dtype):
+def train_discriminator(optimizer, discriminator, loss, real_data, fake_data, dtype, WGAN_training):
     N = real_data.size(0)
     # Reset gradients
     optimizer.zero_grad()
@@ -61,28 +61,58 @@ def train_discriminator(optimizer, discriminator, loss, real_data, fake_data, dt
     # 1.1 Train on Real Data
     prediction_real = discriminator(real_data).type(dtype)
 
+    one = torch.FloatTensor([1]).type(dtype)
+    mone = one * -1
     # Calculate error and backpropagate
-    error_real = loss(prediction_real, ones_target(N).type(dtype))
-    error_real.backward()
+    if WGAN_training:
+        # compute the gradient of the mean, i.e the mean of the gradients by linearity
+        prediction_real.mean().view(1).backward(one)
+    else:
+        error_real = loss(prediction_real, ones_target(N).type(dtype))
+        error_real.backward()
 
     # 1.2 Train on Fake Data
     prediction_fake = discriminator(fake_data).type(dtype)
+    if WGAN_training:
+        # compute the gradient of the mean, i.e the mean of the gradients by linearity
+        prediction_fake.mean().view(1).backward(mone)
+        
     # Calculate error and backpropagate
-    error_fake = loss(prediction_fake, zeros_target(N).type(dtype))
-    error_fake.backward()
+    else:
+        error_fake = loss(prediction_fake, zeros_target(N).type(dtype))
+        error_fake.backward()
+        error_discriminator = error_real - error_fake
 
     # 1.3 Update weights with gradients
     optimizer.step()
 
     # Return error and predictions for real and fake inputs
-    return error_real + error_fake, prediction_real, prediction_fake
+    if WGAN_training:
+        return prediction_real, prediction_fake
+    else:
+        return error_real + error_fake, prediction_real, prediction_fake
 
-
-def train_generator(optimizer, discriminator, loss, fake_data, dtype):
-    N = fake_data.size(0)    # Reset gradients
-    optimizer.zero_grad()    # Sample noise and generate fake data
-    prediction = discriminator(fake_data).type(dtype)    # Calculate error and backpropagate
-    error = loss(prediction, ones_target(N).type(dtype))
-    error.backward()    # Update weights with gradients
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+            
+def train_generator(optimizer, discriminator, loss, generated_data, dtype, WGAN_training):
+    one = torch.FloatTensor([1]).type(dtype)
+    N = generated_data.size(0)    # Reset gradients
+    optimizer.zero_grad()    
+    prediction = discriminator(generated_data).type(dtype)  # Sample noise and generate data 
+    if WGAN_training:
+        prediction.mean().view(1).backward(one)
+    else:
+        error = loss(prediction, ones_target(N).type(dtype))
+        error.backward()    # Update weights with gradients
+    
     optimizer.step()    # Return error
-    return error
+    if WGAN_training:
+        return prediction
+    else:
+        return error
